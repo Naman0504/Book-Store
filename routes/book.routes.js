f4a41b782fff6901f4d4e146fb3a9def2e7945db
Naman0504 <matiyaranaman@gmail.com>
2025-11-27 15:08:19 +0530

    MVC structure

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,53 +1,14 @@
 const express = require('express')
-const {BOOKS} = require('../db/book.js')
 const router = express.Router();
+const controller = require("../controllers/book.controller.js")
 
-router.get('/',(req, res) => {
-    res.status(200).json(BOOKS)
-})
+router.get('/', controller.getAllBooks)
 
-router.get('/:id', (req, res) => {
-    const id = parseInt(req.params.id);
+router.get('/:id',controller.getAllBooks)
 
-    if (isNaN(id)) return res.status(400).json({ erro: `Id must be of number type` })
+router.post('/',controller.createBook )
 
-    const book = BOOKS.find((book) => book.id === id);
-
-    if (!book) return res.status(404).json({ error: `Book with this ${id} is not exist` });
-
-    return res.status(200).json(book)
-
-})
-
-router.post('/', (req, res) => {
-
-    const { title, author } = req.body;
-    console.log(req.body)
-
-    if (!title || title === '') return res.status(400).json({ message: 'Title should not be empty' })
-    if (!author || author === '') return res.status(400).json({ message: 'Author should not be empty' })
-
-    const id = BOOKS.length + 1;
-    const book = { id, title, author }
-    BOOKS.push(book);
-
-    return res.status(201).json({ message: 'Book is successfully added' })
-})
-
-router.delete('/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-
-    if (isNaN(id)) {
-        return res.status(400).json({ erro: `Id must be of number type` })
-    }
-
-    const indexToDeleteBook = BOOKS.findIndex((book) => book.id === id);
-    if (indexToDeleteBook < 0) return res.status(404).json({ messgae: `Book with this ${id} does not exist` });
-
-    BOOKS.splice(indexToDeleteBook, 1)
-
-    return res.status(200).json({ message: `Book with this id:${id} is deleted` })
-})
+router.delete('/:id',controller.deleteById )
 
 
 module.exports = router;
