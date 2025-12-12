e574eabb5668720a6d9dfa531cbcacd73fcda589
Naman0504 <matiyaranaman@gmail.com>
2025-12-12 14:04:06 +0530

    indexing for search

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,38 +1,48 @@
 const booksTable = require("../models/book.model")
 const db = require("../db/index")
-const { eq } = require("drizzle-orm")
+const { eq, ilike, sql } = require("drizzle-orm")
 
-exports.getAllBooks = async function (Req, res) {
+exports.getAllBooks = async function (req, res) {
+    try {
+        const search = req.query.search;
+        console.log(search);
 
-    const books = await db.select().from(booksTable)
-    res.status(200).json(books)
+        if (search) {
+            const books = await db
+                .select()
+                .from(booksTable)
+                .where(
+                    sql`to_tsvector('english', ${booksTable.title} || ' ' || ${booksTable.description}) @@ to_tsquery('english', ${search})`
+                );
 
+            return res.status(200).json(books);   // IMPORTANT: return
+        }
+
+        const books = await db.select().from(booksTable);
+        return res.status(200).json(books);     // second return
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Internal server error" });
+    }
 }
 
 exports.getBookById = async function (req, res) {
-
     const id = req.params.id;
 
-
     const [book] = await db.select().from(booksTable).where(table => eq(table.id, id)).limit(1);
-
     if (!book) return res.status(404).json({ error: `Book with this ${id} is not exist` });
 
     return res.status(200).json(book)
-
-
 }
 
 
 exports.createBook = async function (req, res) {
 
     const { title, description, authorId } = req.body;
-    // console.log(req.body)
 
     if (!title || title === '') return res.status(400).json({ message: 'Title should not be empty' })
-
     const result = await db.insert(booksTable).values({ title, authorId, description }).returning({ id: booksTable.id })
-
 
     return res.status(201).json({ message: 'Book is successfully created', id: result.id })
 }
@@ -40,7 +50,6 @@ exports.createBook = async function (req, res) {
 exports.deleteById = async function (req, res) {
     const id = req.params.id;
     await db.delete(booksTable).where(eq(booksTable.id, id));
-
 
     return res.status(200).json({ message: `Book with this id:${id} is deleted` })
 }
--- models/book.model.js
+++ models/book.model.js
@@ -2,13 +2,19 @@
 const { text } = require("drizzle-orm/pg-core")
 const { pgTable, varchar, uuid } = require("drizzle-orm/pg-core")
 const authorsTable = require("./author.model")
+const { sql } = require("drizzle-orm")
+const { index } = require("drizzle-orm/pg-core")
 
 const booksTable = pgTable("books", {
     id: uuid().primaryKey().defaultRandom(),
     title: varchar({ length: 255 }).notNull(),
     description: text(),
     authorId: uuid().references(() => authorsTable.id)
-})
+}, (table) => ({
+    searchIndexOnTitle: index("title_index").using('gin', sql`to_tsvector('english', ${table.title})`),
+    searchIndexOnDescription: index("description_index").using('gin', sql`to_tsvector('english', ${table.description})`)
+
+}))
 
 
 module.exports = booksTable
